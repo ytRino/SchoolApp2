ec0bca7749b4dd78da84d05a966b7d1b9d8625d0
Yutaro Iino <yutaro.iino@caraquri.com>
2020-09-29 12:42:15 +0900

    LifecycleObserverの例を追加

---
+++ app3/src/main/java/com/caraquri/android/scool/app3/ui/LifecycleFragment.java
@@ -0,0 +1,35 @@
+package com.caraquri.android.scool.app3.ui;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.caraquri.android.scool.app3.R;
+import com.caraquri.android.scool.app3.databinding.LifecycleFragmentBinding;
+
+public class LifecycleFragment extends Fragment {
+
+  public LifecycleFragment() {
+    super(R.layout.lifecycle_fragment);
+  }
+
+  @Override public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    LifecycleFragmentBinding binding = LifecycleFragmentBinding.bind(view);
+
+    // タスクで定期的にTextViewを追加する。
+    // タスクを停止しないと、FragmentがDestroy状態になったときにクラッシュ
+    Task.TaskCallback taskCallback = time -> binding.getRoot().addView(createTextView(time));
+    // タスクをライフサイクルに連動させる
+    getLifecycle().addObserver(new Task(taskCallback));
+  }
+
+  private TextView createTextView(long time) {
+    TextView textView = new TextView(requireContext());
+    textView.setText("Task " + time);
+    return textView;
+  }
+}
---
+++ app3/src/main/java/com/caraquri/android/scool/app3/ui/Task.java
@@ -0,0 +1,63 @@
+package com.caraquri.android.scool.app3.ui;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+
+/**
+ * onResumeからonPauseのあいだまで動作する疑似タスククラス
+ * <pre>
+ * <code>
+ *   lifecycle.addObserver(new Task(time -> yourTask()))
+ * </code>
+ * </pre>
+ */
+class Task implements DefaultLifecycleObserver {
+
+  /**
+   * タスクが動いている間定期的に実行する処理を登録するコールバッククラス
+   */
+  interface TaskCallback {
+    void doWork(long time);
+  }
+
+  public Task(TaskCallback work) {
+    this.work = work;
+  }
+
+  private TaskCallback work;
+
+  private Handler handler = new Handler(Looper.getMainLooper());
+
+  /**
+   * 実行されたあと1秒後に自身を再実行するようhandlerに登録する。
+   */
+  private Runnable worker = new Runnable() {
+    @Override public void run() {
+      long time = System.currentTimeMillis() - start;
+      Log.v("Task", "working! " + time);
+      work.doWork(time);
+      handler.postDelayed(this, 1000);
+    }
+  };
+
+  private long start = 0;
+
+  @Override public void onResume(@NonNull LifecycleOwner owner) {
+    Log.v("Task", "onResume: starting task...");
+    start = System.currentTimeMillis();
+    // 仕事開始
+    handler.postDelayed(worker, 1000);
+  }
+
+  @Override public void onPause(@NonNull LifecycleOwner owner) {
+    Log.v("Task", "onPause: stopping task...");
+    // 仕事終了
+    // ⚠️ コメントアウトするとクラッシュorリーク
+    // java.lang.IllegalStateException: Fragment LifecycleFragment… not attached to a context.
+    handler.removeCallbacks(worker);
+  }
+}
