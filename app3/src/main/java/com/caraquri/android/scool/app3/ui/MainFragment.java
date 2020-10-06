42dd3ae979e5124910f7839ec6d1757468d193a7
Yutaro Iino <yutaro.iino@caraquri.com>
2020-10-06 17:37:13 +0900

    画面遷移を修正

--- app3/src/main/java/com/caraquri/android/scool/app3/ui/MainFragment.java
+++ app3/src/main/java/com/caraquri/android/scool/app3/ui/MainFragment.java
@@ -2,9 +2,11 @@ package com.caraquri.android.scool.app3.ui;
 
 import android.os.Bundle;
 import android.view.View;
+import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import com.caraquri.android.scool.app3.R;
 import com.caraquri.android.scool.app3.databinding.MainFragmentBinding;
 
@@ -18,17 +20,28 @@ public class MainFragment extends Fragment {
     super.onViewCreated(view, savedInstanceState);
 
     MainFragmentBinding binding = MainFragmentBinding.bind(view);
+    // navigation : 自分で画面遷移する場合
     binding.lifecycle.setOnClickListener(v -> open(new LifecycleFragment()));
-    binding.livedata.setOnClickListener(v -> open(new LiveDataFragment()));
-    binding.viewmodel.setOnClickListener(v -> open(new ViewModelFragment()));
-    binding.room.setOnClickListener(v -> open(new RoomFragment()));
-    binding.work.setOnClickListener(v -> open(new WorkFragment()));
+    // navigation : NavControllerを使う場合
+    binding.livedata.setOnClickListener(v -> open(R.id.action_main_to_livedata));
+    // navigation : createNavigateOnClickListenerを使う場合
+    binding.viewmodel.setOnClickListener(navigationListener(R.id.action_main_to_viewmodel));
+    binding.room.setOnClickListener(navigationListener(R.id.action_main_to_room));
+    binding.work.setOnClickListener(navigationListener(R.id.action_main_to_work));
   }
 
   private void open(Fragment fragment) {
     getParentFragmentManager().beginTransaction()
-        .replace(R.id.container, fragment)
+        .replace(R.id.nav_host_fragment, fragment)
         .addToBackStack(null)
         .commit();
+  }
+
+  private void open(@IdRes int actionResId) {
+    Navigation.findNavController(getView()).navigate(actionResId);
+  }
+
+  private View.OnClickListener navigationListener(@IdRes int actionResId) {
+    return Navigation.createNavigateOnClickListener(actionResId);
   }
 }
